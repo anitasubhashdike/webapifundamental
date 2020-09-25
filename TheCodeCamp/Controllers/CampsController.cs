d252b9b65464387053a7f2114c1c1dc08fdfc45f
anitasubhashdike <anita.subhash.dike@gmail.com>
2020-09-25 11:49:03 -0400

    Added Camps Cntroller

---
+++ TheCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using TheCodeCamp.Data;
+
+namespace TheCodeCamp.Controllers
+{
+    public class CampsController : ApiController
+    {
+        private readonly ICampRepository _repositary;
+
+        public CampsController(ICampRepository repositary)
+        {
+            _repositary = repositary;
+        } 
+        // GET api/<controller>
+        public async Task<IHttpActionResult> Get()
+        {
+            try { 
+            var result = await _repositary.GetAllCampsAsync();
+            return Ok( result);
+            }
+            catch(Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
